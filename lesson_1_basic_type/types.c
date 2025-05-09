6f8c3bfff4976356f54747e200fa384373f8e325
matthieu <66738778+Matthieudese78@users.noreply.github.com>
2025-05-09 18:31:19 +0200

    pointers: int *p = &y stores the memory address of the integer y

--- lesson_1_basic_type/types.c
+++ lesson_1_basic_type/types.c
@@ -7,3 +7,16 @@ struct person
   float height;
 };
 
+  // *int is a pointer i.e. an address :
+  int y = 7;
+  // gets the address in memory where y is stored
+  int *p = &y;
+  // the two following syntaxes yield the same output:
+  printf("address %p, value %i \n", p, *p);
+  printf("address %p, value %i \n", &y, y);
+  // now i want to change the value of the stored integer at address p:
+  *p = 14;
+  printf("new value *p  = %i \n", *p);
+  // does it also modifies y?
+  printf("new value y = %i \n", y);
+  // --> yes it does, *p and y are intricate!
